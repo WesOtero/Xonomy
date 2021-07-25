a46f6ea098fd745ac9037bc0c2c367f8b56ffe30
wesotero <wesleyotero@protonmail.com>
2021-07-25 18:43:02 -0400

    first commit

---
+++ src/main/java/com/wesotero/xonomy/XonomyApplication.java
@@ -0,0 +1,13 @@
+package com.wesotero.xonomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XonomyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XonomyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesotero/xonomy/XonomyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesotero.xonomy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XonomyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
